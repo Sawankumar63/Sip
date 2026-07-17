b4664369d77863f9523db02c495ce65434040f2c
Sawankumar63 <sawankumar6369@gmail.com>
2026-07-17 15:23:06 +0530

    sawan

---
+++ JavaScript/1js/script.js
@@ -0,0 +1,54 @@
+// // console.log("Hello ?JavaScript")
+// // let name = "Sawan Kumar";
+// // console.log(name)
+// // let city ="Bhopal";
+// // console.log(city)
+
+// // const country = "India";
+// // console.log(country)
+
+// console.log("Rohn padho loikho ... IAS YAS bano")
+
+// let Mehul = "Rohan";
+// console.log(Mehul)
+
+// Mehul="Jain";
+// console.log(Mehul)
+
+// Mehul = 22
+//  console.log(Mehul+" "+ "Sep")
+// console.log(`${Mehul}sep`)
+
+// const Sawan ="Rohan"
+// console.log(Sawan)
+// Sawan = "Rishav" // conts ke andr re assigned nahi kr sakte hi
+// console.log(Sawan)
+
+// let 1adsd = 30   //Invalid or unexpected token don`t starrt with number
+
+let a=10
+console.log(a)
+
+var b= 30
+console.log(b)
+
+const c =60;
+console.log(c)
+
+{
+    let a=20
+console.log(a)
+
+var b= 50
+console.log(b)
+
+const c =90;
+console.log(c)
+    
+}
+console.log(a)
+console.log(b)
+console.log(c)
+    
+
+
---
+++ JavaScript/1js/type.js
@@ -0,0 +1,16 @@
+let age = 22;
+console.log(typeof(age))
+let price = 199.99;
+console.log(typeof(price))
+
+let name = "Sawan"
+console.log(typeof(name))
+
+let username
+console.log(typeof(username))
+
+let user =null
+console.log(typeof(user))
+
+
+
